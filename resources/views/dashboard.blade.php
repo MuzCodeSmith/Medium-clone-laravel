760be1f0a5e66420a6033b0e52be81a76696af3b
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-18 19:40:07 +0530

    add controller method PostController/index which load dashaboard view fetch categories and  show category on Dashboard tab pills

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,20 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+
+
+                    <ul class="flex flex-wrap text-sm font-medium text-center justify-center text-gray-500 dark:text-gray-400">
+                        @foreach($categories as $category)
+                        <li class="me-2">
+                            <a href="#" class="inline-block px-4 py-3 text-white bg-blue-600 rounded-lg active" aria-current="page">{{$category->name}}</a>
+                        </li>
+                        @endforeach
+                    </ul>
+
                 </div>
             </div>
         </div>

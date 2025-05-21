9fd606111ff9d30325cc95972128e9751f375d57
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-21 08:20:58 +0530

    defined username,bio and image in fillable property

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'bio',
+        'image',
+        'username',
         'email',
         'password',
     ];

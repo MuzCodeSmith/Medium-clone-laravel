667163569787b888b69289c5882002b5f2f2b40e
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-22 09:05:51 +0530

    feat(post/show.blade) : add avatar, clap, content section

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -2,7 +2,79 @@
     <div class="py-4">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-8">
-               {{$post->slug}}
+                <h1 class="text-5xl mb-4">{{$post->title}}</h1>
+                <!-- avatar -->
+                <div class="flex gap-4">
+                    @if($post->user->imageUrl())
+                    <img src="{{Storage::url($post->user->image)}}" class="h-12 w-12 rounded-full" alt="{{$post->title}}">
+                    @else
+                    <img src="https://img.freepik.com/premium-vector/user-profile-icon-vector-image-can-be-used-gaming-ecommerce_120816-406884.jpg?uid=R20122910&ga=GA1.1.1329374159.1713979181&semt=ais_hybrid&w=740" class="h-12 w-12 rounded-full" alt="{{$post->title}}">
+                    @endif
+                    <div>
+                        <div class="flex gap-2">
+                            <h3>{{$post->user->name}}</h3>
+                            &middot;
+                            <a href="#" class="text-emerald-500">Follow</a>
+                        </div>
+                        <div class="flex gap-2 text-sm text-gray-500">
+                            {{$post->readTime()}} min read
+                            &middot;
+                            {{$post->created_at->format('M d, Y')}}
+                        </div>
+                    </div>
+                </div>
+                <!-- avatar -->
+
+               <!-- clap section -->
+               <div class="mt-8 p-4 border-t border-b flex gap-4">
+                    <button class="text-gray-500 flex gap-2 items-center hover:text-gray-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
+                        </svg>
+                        <span class="text-sm">3.4k</span>
+                    </button>
+                    <button class="text-gray-500 flex items-center gap-2 hover:text-gray-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+                        </svg>
+                        <span class="text-sm" >40</span>
+                    </button>
+                </div>
+                <!-- clap section -->
+
+                <!-- post content section -->
+                <div class="mt-8">
+                    <img src="{{$post->imageUrl()}}" class="w-full h-80 object-cover" alt="{{$post->title}}">
+                    <div class="mt-4">
+                        {{$post->content}}
+                    </div>
+                </div>
+                <!-- post content section -->
+
+                <!-- category -->
+                <div class="mt-8">
+                    <span class="px-4 py-2 text-sm bg-gray-200 rounded-full">
+                        {{$post->category->name}}
+                    </span>
+                </div>
+
+                <!-- clap section -->
+                <div class="mt-8 p-4 border-t border-b flex gap-4">
+                    <button class="text-gray-500 flex gap-2 items-center hover:text-gray-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
+                        </svg>
+                        <span class="text-sm">3.4k</span>
+                    </button>
+                    <button class="text-gray-500 flex items-center gap-2 hover:text-gray-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+                        </svg>
+                        <span class="text-sm" >40</span>
+                    </button>
+                </div>
+                <!-- clap section -->
+
             </div>
         </div>
     </div>

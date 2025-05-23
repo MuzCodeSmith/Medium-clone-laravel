84ba5ec0d0ed1f4e633c244e69b58d33b5804337
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-23 08:36:54 +0530

    add Public Profile Page and defined its routing

---
+++ app/Http/Controllers/PublicProfileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PublicProfileController extends Controller
+{
+    public function show(Request $request, User $user){
+        $posts = $user->posts()->latest()->paginate();
+        return view('profile.view',['user'=>$user,'posts'=>$posts]);
+    }
+}

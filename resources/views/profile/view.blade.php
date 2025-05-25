2693fb3a7c19081faf270e6077c7b47ee63bc1dd
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-25 14:40:13 +0530

    feat(Profile/view.blade) : implemented functionality to using alpine js to perfom follow/unfollow

--- resources/views/profile/view.blade.php
+++ resources/views/profile/view.blade.php
@@ -15,18 +15,36 @@
                             @endforelse
                         </div>
                     </div>
-                    <div class="w-[320px] border-l px-8">
+                    <div x-data="{
+                        following: @json($user->isFollowedBy(auth()->user())),
+                        followersCount:{{$user->followers->count()}},
+                        follow() {
+                            axios.post(`/follow/{{$user->id}}`)
+                                .then(res => {
+                                    this.following = !this.following;
+                                    this.followersCount = res.data.followers;
+                                    console.log(res.data);
+                                })
+                                .catch(err => console.log(err));
+                        }
+                    }" class="w-[320px] border-l px-8">
                         <x-user-avatar :user="$user" size="w-24 h-24"></x-user-avatar>
                         <h3 class="mt-4">{{$user->name}}</h3>
-                        <p class="text-gray-500">26k followers</p>
+                        <p class="text-gray-500"> 
+                        <span x-text="followersCount" ></span> followers</p>
                         <p>
                             {{$user->bio}}
                         </p>
+                        @if(auth()->user() && auth()->user()->id !== $user->id)
                         <div class="mt-4">
-                            <button class="bg-emerald-600 rounded-full px-4 py-2 text-white ">
-                                Follow
+                            <button
+                                class="rounded-full px-4 py-2"
+                                x-text="following ? 'Unfollow' : 'Follow'"
+                                :class="following ? 'bg-emerald-200 text-gray-600 hover:bg-emerald-300' : 'bg-emerald-600 text-white hover:bg-emerald-700'"
+                                @click="follow()">
                             </button>
                         </div>
+                        @endif
                     </div>
                 </div>
             </div>

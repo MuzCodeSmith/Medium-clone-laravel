c2fc6c9d09f7e961df651ed21430be2e5603b163
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-25 14:37:14 +0530

    Feat(FolloweContrroller) : add controller method to follow/unfollow user

---
+++ app/Http/Controllers/FollowerController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class FollowerController extends Controller
+{
+    public function followUnfollow(User $user){
+        $user->followers()->toggle(auth()->user());
+        return response()->json([
+            'followers'=>$user->followers()->count(),
+        ]);
+    }
+}

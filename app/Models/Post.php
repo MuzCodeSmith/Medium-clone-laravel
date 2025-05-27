c34ecf51a60eb27e2cd4a48b5a66d3b78544e53b
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-27 22:06:19 +0530

    Setup complete: Spatie Laravel Media lIbrary

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Image\Enums\Fit;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Post extends Model
+class Post extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     protected $fillable = [
         'title',
@@ -19,29 +24,44 @@ class Post extends Model
         'category_id',
         'published_at'
     ];
+
+
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('preview')
+            ->width(400)
+            ->nonQueued();
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    public function readTime($wordsPerMinute = 100){
+    public function readTime($wordsPerMinute = 100)
+    {
         $wordCount = str_word_count(strip_tags($this->content));
-        $minutes = ceil($wordCount/$wordsPerMinute);
+        $minutes = ceil($wordCount / $wordsPerMinute);
 
         return max(1, $minutes);
     }
 
-    public function imageUrl(){
-        if($this->image){
+    public function imageUrl()
+    {
+        if ($this->image) {
             return Storage::url($this->image);
         }
     }
 
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function claps(){
-        return $this->hasMany(Clap::class);        
+    public function claps()
+    {
+        return $this->hasMany(Clap::class);
     }
 }

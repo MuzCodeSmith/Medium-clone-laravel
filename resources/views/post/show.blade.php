91768b531510f59277252c89f0b4bbb31e8ee314
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-29 23:18:33 +0530

    feat(show.post) : add Edit and Delete Button in View Post Page

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -25,7 +25,7 @@
 
                 @if($post->user_id == Auth::id())
                 <div class="mt-8 py-4 border-t border-b border-gray-200">
-                    <x-primary-button>
+                    <x-primary-button href="{{route('post.edit',$post->slug)}}" >
                         Edit Post
                     </x-primary-button>
                     <form class="inline-block" action="{{route('post.destroy',$post)}}" method="post">

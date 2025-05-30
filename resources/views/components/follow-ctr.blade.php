f04fd9c7d4d236638a6f257fe1bbc60b08191ed4
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-31 02:39:55 +0530

    refactor follow logic

--- resources/views/components/follow-ctr.blade.php
+++ resources/views/components/follow-ctr.blade.php
@@ -7,10 +7,9 @@
                                 .then(res => {
                                     this.following = !this.following;
                                     this.followersCount = res.data.followers;
-                                    console.log(res.data);
                                 })
                                 .catch(err => console.log(err));
                         }
                     }" class="w-[320px] border-l px-8">
-                    {{$slot}}
+    {{$slot}}
 </div>
